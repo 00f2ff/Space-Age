39802f93868e15b35d3d5b7ce844984f196c2e1c
Duncan McIsaac <dbmcisaac1@gmail.com>
2016-05-23 18:58:54 -0400

    finished initial scope of planet class

--- planet.js
+++ planet.js
@@ -103,6 +103,102 @@ var planetData = {
 			tritiumMine: 0
 		}
 	},
+	aqua: {
+		mineRates: {
+			crystalMine: 1,
+			steelMine: 0,
+			titaniumMine: 0.5,
+			tritiumMine: 2.5
+		},
+		powerRates: {
+			hydropowerPlant: 1.5,
+			thermalPowerPlant: 0.5,
+			windPowerPlant: 1.5,
+			planetaryPowerGenerator: 0,
+			liquidPowerPlant: 1,
+			furnacePowerPlant: 0,
+			nuclearPowerPlant: 1.5
+		}
+		buildingSlots: 7,
+		mineSlots: {
+			crystalMine: 1,
+			steelMine: 0,
+			titaniumMine: 1,
+			tritiumMine: 2
+		}
+	},
+	airy: {
+		mineRates: {
+			crystalMine: 3,
+			steelMine: 0,
+			titaniumMine: 0,
+			tritiumMine: 1
+		},
+		powerRates: {
+			hydropowerPlant: 1,
+			thermalPowerPlant: 0,
+			windPowerPlant: 2.9,
+			planetaryPowerGenerator: 0,
+			liquidPowerPlant: 0.7,
+			furnacePowerPlant: 0.7,
+			nuclearPowerPlant: 0.7
+		}
+		buildingSlots: 3,
+		mineSlots: {
+			crystalMine: 3,
+			steelMine: 0,
+			titaniumMine: 0,
+			tritiumMine: 1
+		}
+	},
+	crag: {
+		mineRates: {
+			crystalMine: 0,
+			steelMine: 1.5,
+			titaniumMine: 2.5,
+			tritiumMine: 0
+		},
+		powerRates: {
+			hydropowerPlant: 0,
+			thermalPowerPlant: 1,
+			windPowerPlant: 1,
+			planetaryPowerGenerator: 0,
+			liquidPowerPlant: 0.5,
+			furnacePowerPlant: 2,
+			nuclearPowerPlant: 1.5
+		}
+		buildingSlots: 10,
+		mineSlots: {
+			crystalMine: 0,
+			steelMine: 3,
+			titaniumMine: 3,
+			tritiumMine: 0
+		}
+	},
+	outcrop: {
+		mineRates: {
+			crystalMine: 0.5,
+			steelMine: 1,
+			titaniumMine: 1.5,
+			tritiumMine: 1
+		},
+		powerRates: {
+			hydropowerPlant: 0.5,
+			thermalPowerPlant: 0.5,
+			windPowerPlant: 1.5,
+			planetaryPowerGenerator: 0,
+			liquidPowerPlant: 1,
+			furnacePowerPlant: 1.5,
+			nuclearPowerPlant: 1
+		}
+		buildingSlots: 12,
+		mineSlots: {
+			crystalMine: 1,
+			steelMine: 1,
+			titaniumMine: 2,
+			tritiumMine: 0
+		}
+	}
 }
 
 /*
@@ -111,62 +207,24 @@ var planetData = {
 function Planet(type, sun) {
 	this.type = type;
 	this.sun = sun;
-	this.init();
-
-	function init() {
-		this.resources = {
-			crystal: 1000,
-			steel: 1000,
-			titanium: 500,
-			tritium: 1000
-		}
-		this.power = 25;
-		this.mineRates = this.sun.mineRates;
-	}
-}
-var planetData = {
-	resources: {
-		crystal: undefined,
-		steel: undefined,
-		titanium: undefined,
-		tritium: undefined
-	},
-	mineRates: {
-		crystalMine: undefined,
-		steelMine: undefined,
-		titaniumMine: undefined,
-		tritiumMine: undefined
-	},
-	powerRates: {
-		hydropowerPlant: undefined,
-		thermalPowerPlant: undefined,
-		windPowerPlant: undefined,
-		planetaryPowerGenerator: 1,
-		liquidPowerPlant: 1,
-		furnacePowerPlant: 1,
-		nuclearPowerPlant: 1
-	}
-	energy: undefined,
-	structures: undefined,
-	maxStructures: undefined,
-	buildings: {
+	this.buildings = {
 		mine: {
-			crystalMine: [1],
-			steelMine: [1],
-			titaniumMine: [1],
-			tritiumMine: [1]
+			crystalMine: [],
+			steelMine: [],
+			titaniumMine: [],
+			tritiumMine: []
 		},
 		storage: {
-			crystalStorage: [1],
-			steelStorage: [1],
-			titaniumStorage: [1],
-			tritiumStorage: [1]
+			crystalStorage: [],
+			steelStorage: [],
+			titaniumStorage: [],
+			tritiumStorage: []
 		},
 		power: {
 			hydropowerPlant: [],
 			thermalPowerPlant: [],
 			windPowerPlant: [],
-			planetaryPowerGenerator: [1],
+			planetaryPowerGenerator: [],
 			liquidPowerPlant: [],
 			furnacePowerPlant: [],
 			nuclearPowerPlant: []
@@ -182,18 +240,61 @@ var planetData = {
 		},
 		defense: {
 			defenseFactory: []
+		},
+		craft: {
+
+		},
+		technology: {
+
+		},
+		reference: {
+
 		}
-	},
-	craft: {
-
-	},
-	technology: {
-
-	},
-	reference: {
-
 	}
+	this.init();
+
+	function init() {
+		// assign initial values
+		this.resources = {
+			crystal: 1000,
+			steel: 1000,
+			titanium: 500,
+			tritium: 1000
+		}
+		this.power = 25;
+		// assign mineRates from sun
+		this.mineRates = this.sun.mineRates;
+		// modify mineRates from planet type
+		for (var rate in this.mineRates) {
+			if (!this.mineRates.hasOwnProperty(key)) continue;
+			this.mineRates[rate] *= planetData[type].mineRates[rate];
+		}
+		// assign planet type data
+		this.powerRates = planetData[type].powerRates;
+		this.buildings = 0;
+		this.buildingSlots = planetData[type].buildingSlots;
+		this.mineSlots = planetData[type].mineSlots;
+		// build specific buildings only for Super Terra
+		if (type === 'superTerra') {
+			this.buildings.mine = {
+				crystalMine: [1],
+				steelMine: [1],
+				titaniumMine: [1],
+				tritiumMine: [1]
+			}
+			this.buildings.storage = {
+				crystalStorage: [1],
+				steelStorage: [1],
+				titaniumStorage: [1],
+				tritiumStorage: [1]
+			}
+			this.buildings.power.planetaryPowerGenerator = [1];
+		}
+	}
+
+	
 }
+
 
 /*
  * The way this is going to work is there will be a master building file, but I'll keep track of level and quantity in this file
