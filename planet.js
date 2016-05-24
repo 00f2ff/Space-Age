ae25f21e85fb9896d50261c9ab7ed048ccc67b00
Duncan McIsaac <dbmcisaac1@gmail.com>
2016-05-23 20:32:38 -0400

    renamed some resource-specific buildings for clarity, and added more data-driven building code to planet

--- planet.js
+++ planet.js
@@ -3,21 +3,21 @@
 function Sun() {
 	this.strength = Math.round(Math.random() * 4 + 1);
 	if (this.strength === 3) {
-		this.mineRates = {
+		this.mineMultipliers = {
 			crystalMine: 1,
 			steelMine: 1,
 			titaniumMine: 1,
 			tritiumMine: 1
 		}
 	} else if (this.strength > 3) {
-		this.mineRates = {
+		this.mineMultipliers = {
 			crystalMine: 1,
 			steelMine: 1 - (this.strength - 3) * 0.05,
 			titaniumMine: 1 - (this.strength - 3) * 0.05,
 			tritiumMine: 1 + (this.strength - 3) * 0.05
 		}
 	} else {
-		this.mineRates = {
+		this.mineMultipliers = {
 			crystalMine: 1,
 			steelMine: 1 + (this.strength - 3) * 0.05,
 			titaniumMine: 1 + (this.strength - 3) * 0.05,
@@ -32,13 +32,13 @@ function Sun() {
  */
 var planetData = {
 	superTerra: {
-		mineRates: {
+		mineMultipliers: {
 			crystalMine: 2,
 			steelMine: 2,
 			titaniumMine: 2,
 			tritiumMine: 2
 		},
-		powerRates: {
+		powerMultipliers: {
 			hydropowerPlant: 1,
 			thermalPowerPlant: 1,
 			windPowerPlant: 1,
@@ -56,13 +56,13 @@ var planetData = {
 		}
 	},
 	terra: {
-		mineRates: {
+		mineMultipliers: {
 			crystalMine: 1,
 			steelMine: 1,
 			titaniumMine: 1,
 			tritiumMine: 1
 		},
-		powerRates: {
+		powerMultipliers: {
 			hydropowerPlant: 1,
 			thermalPowerPlant: 1,
 			windPowerPlant: 1,
@@ -80,13 +80,13 @@ var planetData = {
 		}
 	},
 	volcanic: {
-		mineRates: {
+		mineMultipliers: {
 			crystalMine: 1,
 			steelMine: 1.5,
 			titaniumMine: 1.5,
 			tritiumMine: 0
 		},
-		powerRates: {
+		powerMultipliers: {
 			hydropowerPlant: 0,
 			thermalPowerPlant: 2,
 			windPowerPlant: 1.5,
@@ -104,13 +104,13 @@ var planetData = {
 		}
 	},
 	aqua: {
-		mineRates: {
+		mineMultipliers: {
 			crystalMine: 1,
 			steelMine: 0,
 			titaniumMine: 0.5,
 			tritiumMine: 2.5
 		},
-		powerRates: {
+		powerMultipliers: {
 			hydropowerPlant: 1.5,
 			thermalPowerPlant: 0.5,
 			windPowerPlant: 1.5,
@@ -128,13 +128,13 @@ var planetData = {
 		}
 	},
 	airy: {
-		mineRates: {
+		mineMultipliers: {
 			crystalMine: 3,
 			steelMine: 0,
 			titaniumMine: 0,
 			tritiumMine: 1
 		},
-		powerRates: {
+		powerMultipliers: {
 			hydropowerPlant: 1,
 			thermalPowerPlant: 0,
 			windPowerPlant: 2.9,
@@ -152,13 +152,13 @@ var planetData = {
 		}
 	},
 	crag: {
-		mineRates: {
+		mineMultipliers: {
 			crystalMine: 0,
 			steelMine: 1.5,
 			titaniumMine: 2.5,
 			tritiumMine: 0
 		},
-		powerRates: {
+		powerMultipliers: {
 			hydropowerPlant: 0,
 			thermalPowerPlant: 1,
 			windPowerPlant: 1,
@@ -176,13 +176,13 @@ var planetData = {
 		}
 	},
 	outcrop: {
-		mineRates: {
+		mineMultipliers: {
 			crystalMine: 0.5,
 			steelMine: 1,
 			titaniumMine: 1.5,
 			tritiumMine: 1
 		},
-		powerRates: {
+		powerMultipliers: {
 			hydropowerPlant: 0.5,
 			thermalPowerPlant: 0.5,
 			windPowerPlant: 1.5,
@@ -210,16 +210,16 @@ function Planet(type, sun) {
 	this.resourceTypes = ["crystal", "steel", "titanium", "tritium"];
 	this.buildings = {
 		mine: {
-			crystalMine: [],
-			steelMine: [],
-			titaniumMine: [],
-			tritiumMine: []
+			crystal: [],
+			steel: [],
+			titanium: [],
+			tritium: []
 		},
 		storage: {
-			crystalStorage: [],
-			steelStorage: [],
-			titaniumStorage: [],
-			tritiumStorage: []
+			crystal: [],
+			steel: [],
+			titanium: [],
+			tritium: []
 		},
 		power: {
 			hydropowerPlant: [],
@@ -262,32 +262,46 @@ function Planet(type, sun) {
 			titanium: 500,
 			tritium: 1000
 		}
+		this.mineRates = {
+			crystal: 0,
+			steel: 0,
+			titanium: 0,
+			tritium: 0
+		}
+		this.storage = {
+			crystal: 2000,
+			steel: 2000,
+			titanium: 2000,
+			tritium: 2000
+		}
 		this.power = 25;
-		// assign mineRates from sun
-		this.mineRates = this.sun.mineRates;
-		// modify mineRates from planet type
-		for (var rate in this.mineRates) {
-			if (!this.mineRates.hasOwnProperty(key)) continue;
-			this.mineRates[rate] *= planetData[type].mineRates[rate];
+		// assign mineMultipliers from sun
+		this.mineMultipliers = this.sun.mineMultipliers;
+		// modify mineMultipliers from planet type
+		for (var mult in this.mineMultipliers) {
+			if (!this.mineMultipliers.hasOwnProperty(mult)) continue;
+			this.mineMultipliers[mult] *= planetData[type].mineMultipliers[mult];
 		}
 		// assign planet type data
-		this.powerRates = planetData[type].powerRates;
+		this.powerMultipliers = planetData[type].powerMultipliers;
 		this.usedBuildingSlots = 0;
 		this.maxBuildingSlots = planetData[type].maxBuildingSlots;
 		this.mineSlots = planetData[type].mineSlots;
 		// build specific buildings only for Super Terra
 		if (type === 'superTerra') {
 			this.buildings.storage = {
-				crystalStorage: [1],
-				steelStorage: [1],
-				titaniumStorage: [1],
-				tritiumStorage: [1]
+				crystal: [1],
+				steel: [1],
+				titanium: [1],
+				tritium: [1]
 			}
 			this.usedBuildingSlots = 4;
 			this.buildings.power.planetaryPowerGenerator = [1];
 		}
 	}
 }
+
+//// BUILDING FUNCTIONS ////
 
 Planet.prototype.canUpgradeBuilding = function(category, name, index) {
 	var nextLevelIndex, i, r;
@@ -307,8 +321,13 @@ Planet.prototype.canUpgradeBuilding = function(category, name, index) {
 
 Planet.prototype.upgradeBuilding = function(category, name, index) {
 	if (this.canUpgradeBuilding(category, name, index)) {
+		// increase level
 		this.buildings[category][name][index]++;
+		// modify power
+		if ('category' === power) this.power += buildingData[category][name].production[this.buildings[category][name][index]];
+		else this.power -= buildingData[category][name].power[this.buildings[category][name][index]];
 	}
+	this.updateUIVariables(category, name);
 }
 
 Planet.prototype.canBuyBuilding = function(category, name) {
@@ -325,7 +344,47 @@ Planet.prototype.buyBuilding = function(category, name) {
 	if (canBuyBuilding(category, name)) {
 		this.buildings[category][name].push(1); // add a level 1 building
 		if (category !== 'mine') this.usedBuildingSlots++;
+		// modify power
+		if ('category' === power) this.power += buildingData[category][name].production[this.buildings[category][name][index]];
+		else this.power -= buildingData[category][name].power[this.buildings[category][name][index]];
 	}
+	this.updateUIVariables(category, name);
+}
+
+//// RESOURCE FUNCTIONS ////
+
+// Helper to recalculate mineRates / storage (power is already handled in buy / upgrade functions)
+Planet.prototype.updateUIVariables = function(category, name) {
+	switch(category) {
+		case 'mine':
+			this.mineRates[name] = this.sum('mine', name, 'production') * this.mineMultipliers[name];
+			break;
+		case 'storage':
+			this.storage[name] = this.sum('storage', name, 'storage');
+			break;
+		default:
+			break;
+	}
+}
+
+// Helper to calculate a sum of a particular attribute given a building level array
+Planet.prototype.sum = function(category, name, attribute) {
+	// Sse ECMAScript 6 for inlined reduce function
+	return this.buildings[category][name].reduce((a, b) => a + buildingData[category][name][attribute][b], 0);
+}
+
+// This gets called on each game loop
+Planet.prototype.dataLoop = function(cyclesPerSecond) {
+	// modify resources
+	var r, i, increment;
+	for (i = 0; i < resourceTypes.length; i++) {
+		r = resourceTypes[i];
+		increment = this.mineRates[r] / 60 / cyclesPerSecond;
+		// increment resource if less than storage, otherwise set to storage
+		if (this.resources[r] + increment < this.storage[r]) this.resources[r] += increment;
+		else this.resources[r] = this.storage[r];
+	}
+	// economy, fleet, defense, technology? (whatever is changed incrementally)
 }
 
 /*
